e4829897705fe03fe672e536a3cb30e77f647bcd
SansNeal <62467577+SansNeal@users.noreply.github.com>
2020-04-21 15:28:01 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -112,7 +112,7 @@ async function getBackgroundImg(){
     var hour = datetime.slice(11,13);
     
     if(hour>=0600 && hour<=1900){
-        bg = "sprites/bg1.png";
+        bg = "sprites/bg.png";
     }
     else{
         bg = "sprites/bg2.jpg";
